099dc6bc22f936c7541cd45e83a67db357d83ff1
Paraskevas Petsanas <42607603+ppel123@users.noreply.github.com>
2023-09-03 19:44:59 +0300

    Update CreateLocalAdmin-Remediation.ps1

--- Windows LAPS – The new powerful era/CreateLocalAdmin-Remediation.ps1
+++ Windows LAPS – The new powerful era/CreateLocalAdmin-Remediation.ps1
@@ -1,5 +1,5 @@
 ﻿# Username and Password
 $username = "WindowsLocalAdmin"
-$password = ConvertTo-SecureString "WindowsLocalAdmin123!" -AsPlainText -Force
+$password = ConvertTo-SecureString "xxx" -AsPlainText -Force
 # Creating the user
 New-LocalUser -Name "$username" -Password $password -FullName "$username" -Description "LAPS Admin"
