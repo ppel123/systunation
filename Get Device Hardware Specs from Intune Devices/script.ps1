45698f863f57e8bd45322cbb9ccdc61f1c29be1d
Paraskevas Petsanas <42607603+ppel123@users.noreply.github.com>
2023-09-16 16:57:25 +0300

    Get Device Hardware Specs from Intune Devices

---
+++ Get Device Hardware Specs from Intune Devices/script.ps1
@@ -0,0 +1,30 @@
+$valueToReport = ""
+
+$processorSpecs = gcim win32_processor
+
+$processorName = $processorSpecs.Name
+$valueToReport += ("Processor Name: " + $processorName.Trim() + "||")
+$processorSpeed = [string]([math]::round(($processorSpecs.CurrentClockSpeed /1000),2)) + 'ghz'
+$valueToReport += ("Processor Speed: " + $processorSpeed.Trim() + "||")
+$processorCores = $processorSpecs.NumberOfCores
+$valueToReport += ("Processor Cores: " + $processorCores.ToString() + "||")
+$processorThreads = $processorSpecs.ThreadCount
+$valueToReport += ("Processor Threads: " + $processorThreads.ToString() + "||")
+
+$storage = ""
+
+$hdd | ForEach{$storage += $_.caption + ", Capacity: " + [math]::round(($_.Size / 1GB),'2') + "GB - "  }
+$valueToReport += ("Storage Info: " + $storage.Trim() + "||")
+
+$ramTotal = "{0:N2}" -f (((gcim CIM_PhysicalMemory | select -ExpandProperty Capacity) | measure -Sum).sum /1gb ) + ' GB'
+$valueToReport += ("RAM Size: " + $ramTotal.Trim() + "||")
+$computerName = $env:ComputerName
+$valueToReport += ("Computer Name: " + $computerName.Trim() + "||")
+$systemType = gcim win32_operatingsystem | select -ExpandProperty OSArchitecture
+$valueToReport += ("Computer Name: " + $systemType.Trim() + "||")
+$serial = gcim win32_bios | select -expandproperty serialnumber
+$valueToReport += ("Serial Number: " + $serial.Trim() + "||")
+$os = gcim win32_operatingsystem | select -expandproperty caption
+$valueToReport += ("OS: " + $os.Trim() + "||")
+
+Write-Host $valueToReport
