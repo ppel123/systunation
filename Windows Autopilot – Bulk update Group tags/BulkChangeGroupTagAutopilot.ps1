82c3faebf19c4e5b80f690799385e15ff2e9a88f
Paraskevas Petsanas <42607603+ppel123@users.noreply.github.com>
2023-12-01 22:01:51 +0200

    added bulk update group tag script

---
+++ Windows Autopilot – Bulk update Group tags/BulkChangeGroupTagAutopilot.ps1
@@ -0,0 +1,18 @@
+﻿# Install-Module -Name Microsoft.Graph
+# Install-Module -Name WindowsAutoPilotIntune
+# Be sure to have the correct permissions to perform Autopilot related operations
+
+Connect-MgGraph
+
+# Specify the new group tag here
+$Grouptag = "NewGroupTag" 
+
+# Get the serial numbers of the devices that we want to change group tag
+$serialsTXTFilePathLocation = Read-Host "Enter the txt path location containing the serial numbers:"
+$serials = Get-Content -Path $serialsTXTFilePathLocation
+
+foreach($serial in $serials)
+{
+    Write-Host "Changing Group tag for: $serial"
+    Get-AutopilotDevice -serial $serial | Set-AutopilotDevice -groupTag $Grouptag
+}
