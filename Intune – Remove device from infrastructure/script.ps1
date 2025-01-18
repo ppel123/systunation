db1a8be7da0bb6db60968df524318f28511271c8
Paraskevas Petsanas <42607603+ppel123@users.noreply.github.com>
2025-01-18 13:42:34 +0200

    Update script.ps1

--- Intune – Remove device from infrastructure/script.ps1
+++ Intune – Remove device from infrastructure/script.ps1
@@ -1,29 +1,58 @@
-﻿Install-Module -Name Microsoft.Graph.Intune
-Install-Module -Name WindowsAutopilotIntune
-Install-Module -Name AzureAD
+# Install needed modules - if required
+# Install-Module -Name Microsoft.Graph
+# Install-Module -Name Microsoft.Graph.Intune
+# Install-Module -Name Microsoft.Graph.DeviceManagement
 
-Connect-MSGraph
-Connect-AzureAD
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes "Device.ReadWrite.All", "DeviceManagementManagedDevices.ReadWrite.All", "Directory.ReadWrite.All", "DeviceManagementServiceConfig.ReadWrite.All"
 
-$importedSerials = Get-Content -Path "C:\Users\Public\serials2delete.txt"
+# Import serials from a txt file
+$importedSerials = Get-Content -Path "C:\Temp\serials2delete.txt"
 
+# Iterate through every serial and delete record
 foreach ($serial in $importedSerials){
-
-    $info = Get-IntuneManagedDevice -Filter "serialNumber eq '$serial'" | Select deviceName, serialNumber, lastSyncDateTime, complianceState, managedDeviceId
-    Write-Host "Starting deletion of device $($info.deviceName)"
-
-    Write-Host "Deleting Intune Record for $serial"
-    Remove-IntuneManagedDevice -managedDeviceId $info.managedDeviceId -Verbose -ErrorAction Stop
-    Start-Sleep -Seconds 5
-
-    Write-Host "Deleting Autopilot Record for $serial"
-    Get-AutopilotDevice | Where-Object SerialNumber -eq $serial | Remove-AutopilotDevice
-    Start-Sleep -Seconds 5
     
-    $azureADinfo = Get-AzureADDevice -Filter "DisplayName eq '$($info.deviceName)'" | select *
-    Write-Host "Deleting Azure AD Record for $serial"
-    Remove-AzureADDevice -ObjectId $azureADinfo.ObjectId
-    Start-Sleep -Seconds 5
-}
+    try {
+        # Get device info from Intune
+        $device = Get-MgDeviceManagementManagedDevice -Filter "serialNumber eq '$serial'" 
+        if ($device) {
+            Write-Host "Starting deletion of device $($device.DeviceName)"
+        
+            # Delete Intune record using the Intune Device ID as identifier
+            try {
+                Write-Host "Deleting Intune Record for $serial"
+                Remove-MgDeviceManagementManagedDevice -ManagedDeviceId $device.Id -Verbose -ErrorAction Stop
+                Start-Sleep -Seconds 10
+            } catch {
+                Write-Host "Error deleting Intune record for $serial : $_" -ForegroundColor Red
+            }
 
-# asd
+            # Delete Autopilot device using serial as identifier
+            try {
+                Write-Host "Deleting Autopilot Record for $serial"
+                $autopilotDevice = Get-MgDeviceManagementWindowsAutopilotDeviceIdentity | Where-Object { $_.SerialNumber -eq $serial }
+                if ($autopilotDevice) {
+                    Remove-MgDeviceManagementWindowsAutopilotDeviceIdentity -WindowsAutopilotDeviceIdentityId $autopilotDevice.Id
+                }
+                Start-Sleep -Seconds 10
+            } catch {
+                Write-Host "Error deleting Autopilot record for $serial : $_" -ForegroundColor Red
+            }
+            
+            # Delete Entra ID record using device name as identifier
+            try {
+                Write-Host "Fetching Entra ID Device for $serial"
+                $entraIDData = Get-MgDevice -Filter "DeviceId eq '$device.AzureAdDeviceId'"
+                Write-Host "Deleting Entra ID Record for $serial"
+                Remove-MgDevice -DeviceId $entraIDData.Id
+                Start-Sleep -Seconds 10
+            } catch {
+                Write-Host "Error deleting Entra ID record for $serial : $_" -ForegroundColor Red
+            }
+        } else {
+            Write-Host "No device found with serial number $serial"
+        }
+    } catch {
+        Write-Host "Error processing serial number $serial : $_" -ForegroundColor Red
+    }
+}
